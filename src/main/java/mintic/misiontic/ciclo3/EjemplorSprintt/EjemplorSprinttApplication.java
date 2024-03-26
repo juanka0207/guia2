0c64867b66471207fc59c5198bd838bcd8520550
juanka0207 <juan.gaviria@unicolombo.edu.co>
2024-03-25 19:24:31 -0500

    primera actualizacion de la guia2

---
+++ src/main/java/mintic/misiontic/ciclo3/EjemplorSprintt/EjemplorSprinttApplication.java
@@ -0,0 +1,13 @@
+package mintic.misiontic.ciclo3.EjemplorSprintt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjemplorSprinttApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjemplorSprinttApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mintic/misiontic/ciclo3/EjemplorSprintt/EjemplorSprinttApplicationTests.java
@@ -0,0 +1,13 @@
+package mintic.misiontic.ciclo3.EjemplorSprintt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjemplorSprinttApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
